0ae132b24e437e68e0795f2cf24290797620272f
mirndlazaro <mirandaalazaro@gmail.com>
2021-03-29 09:49:23 -0300

    :construction: Página principal

---
+++ view/index.php
@@ -0,0 +1,21 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Compra Certa</title>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+        <h1>
+            Home
+        </h1>
+    </body>
+</html>
