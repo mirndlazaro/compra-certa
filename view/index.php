07d1f70d924c4ac148bb5088f82abea614034946
mirndlazaro <mirandaalazaro@gmail.com>
2021-03-29 11:21:16 -0300

    :construction: Adição de atributos da página

--- view/index.php
+++ view/index.php
@@ -14,8 +14,33 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
     </head>
     <body>
-        <h1>
-            Home
-        </h1>
+        <header>
+            <nav>
+                <figure>
+                    <img src="./imgs/logo.png" alt="">
+                </figure>
+            </nav>
+        </header>
+        
+        <main>
+            <div id="welcome-message">
+                <h1>Faça compras no seu mercado favorito com a comidade de casa</h1>
+            </div>
+            <div id="action-buttons">
+                <button id="signup">
+                    <a href="./pages/cliente/cadastro.php">Cadastre-se e comece a comprar</a>
+                </button>
+    
+                <button id="signin">
+                    <a href="./pages/cliente/login.php">Já possui cadastro? <b>Faça login</b></a>
+                </button>
+            </div>
+        </main>
+
+        <footer>
+            <button id="home">
+                <a href="./pages/home.php">Acesse aqui nossos produtos</a>
+            </button>
+        </footer>
     </body>
 </html>
